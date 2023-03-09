5228b365808918881032be4f9d6841f988bff78f
LeBogo <lebogomc@gmail.com>
2023-03-09 16:29:53 +0100

    Fix maps not being serialized correctly

--- src/BaseEvent.ts
+++ src/BaseEvent.ts
@@ -2,11 +2,27 @@ export class BaseEvent {
     constructor(public name: string) {}
 
     stringify(): string {
-        return JSON.stringify(this);
+        return JSON.stringify(this, (key, value) => {
+            if (value instanceof Map) {
+                return {
+                    dataType: "Map",
+                    value: Array.from(value.entries()),
+                };
+            } else {
+                return value;
+            }
+        });
     }
 
     static parse<T extends BaseEvent>(eventString: string): T {
-        const parsed = JSON.parse(eventString);
+        const parsed = JSON.parse(eventString, (key, value) => {
+            if (typeof value === "object" && value !== null) {
+                if (value.dataType === "Map") {
+                    return new Map(value.value);
+                }
+            }
+            return value;
+        });
         const event = new BaseEvent(parsed.name);
         return Object.assign(event, parsed) as T;
     }
